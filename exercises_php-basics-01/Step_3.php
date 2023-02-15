c68f0eae386d07ce0878316336998fe3bbdc8d35
Aya Shahal <ayashahal055@gmail.com>
2023-02-15 19:36:28 +0200

    Step_3

---
+++ exercises_php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$variable_name ="aya";
+$variable_email="aya@gmail.com";
+echo "My name is " . $variable_name . " and my email is " . $variable_email . "</br>";
+$height = 10;
+$width = 20;
+$area = $height * $width;
+echo "Area: " . $area;
+?>
