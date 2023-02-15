89ca1e1b2291efd776210a6efd84c5b6dde6f627
Aya Shahal <ayashahal055@gmail.com>
2023-02-15 20:30:28 +0200

    Step_7

---
+++ exercises_php-basics-01/Step_7.php
@@ -0,0 +1,12 @@
+<?php
+$birth_year="2002";
+$current_year="2023";
+
+$age=$current_year - $birth_year;
+if($age >= 18){
+    echo "you can drive";
+}
+else{
+    echo "You still a kid, go and play GTA";
+}
+?>
