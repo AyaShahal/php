121f2df7d32b0903f2cd31a2b9707ed21a52ebaf
Aya Shahal <ayashahal055@gmail.com>
2023-02-15 19:47:21 +0200

    Step_4

---
+++ exercises_php-basics-01/Step_4.php
@@ -0,0 +1,6 @@
+<?php 
+$name = 'aya';
+$position = 'Full stack developer';
+$github_url = 'https://github.com/gk-git';
+echo "Hello, I'm " . $name . ". I'm a " . $position . ". Please check my GitHub link: " . $github_url;
+?>
