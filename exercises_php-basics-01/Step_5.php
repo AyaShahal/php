315f4287391bd74e020ccc1fa881e3fcbaa9dc81
Aya Shahal <ayashahal055@gmail.com>
2023-02-15 20:02:35 +0200

    Step_5

---
+++ exercises_php-basics-01/Step_5.php
@@ -0,0 +1,10 @@
+<?php
+$course_name='lamp stack';
+$enrolled_students='40';
+$price = 49.99;
+$on_discount = true;
+echo "Course Name: " . $course_name . "<br>";
+echo "Enrolled Students: " . $enrolled_students . "<br>";
+echo "Price: $" . $price . "<br>";
+echo "On Discount: " . ($on_discount ? 'Yes' : 'No') . "<br>";
+?>
